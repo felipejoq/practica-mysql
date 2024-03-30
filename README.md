2533ab41478fb72dbc651f23d43b0dc85cddd333
felipejoq <felipejofreq@gmail.com>
2024-03-30 11:59:56 -0300

    Práctica con bases de datos relacionales

---
+++ README.md
@@ -0,0 +1,12 @@
+# 👨‍💻 Practicando con SQL y MySQL
+
+Pequeño ejercicio para practicar sentencias simples de SQL usando MySQL como gestor de base de datos y así reforzar
+conocimientos en el uso de Bases de Datos relaciones, manipulación y consulta de datos, creación de tablas.
+
+El [script](script.sql) contiene las sentencias básicas para crear las entidades y relaciones necesarias para el ejercicio.
+
+Las relaciones que se crean quedan expresadas en el siguiente modelo entidad relación, que de igual manera queda respaldado
+en el siguiente archivo compatible para la web draw.io [diagrama_er_billetera_virtual.drawio](diagrama_er_billetera_virtual.drawio),
+la imágen PNG [diagrama ER de la DB](diagrama_er_billetera_virtual.png) y un archivo PDF del mismo [diagrama ER de la DB](diagrama_er_billetera_virtual.pdf).
+
+![Modelo entidad relación](diagrama_er_billetera_virtual.png)
---
+++ script.sql
@@ -0,0 +1,150 @@
+-- Esta sentencia crea la base de datos:
+-- CREATE DATABASE alke_wallet;
+
+-- Esta sentencia selecciona la db que queremos usar:
+-- USE alke_wallet;
+
+-- Creación de la tabla User
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    balance DECIMAL(10, 2) DEFAULT 0.00 NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_roles (
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+-- Creación de la tabla Currency
+CREATE TABLE IF NOT EXISTS currencies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    symbol VARCHAR(10) NOT NULL
+);
+
+-- Tabla de tipo de transacciones
+CREATE TABLE IF NOT EXISTS transaction_types (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Creación de la tabla Transaction
+CREATE TABLE IF NOT EXISTS transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    total DECIMAL(10, 2) NOT NULL,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    sender_user_id INT NOT NULL,
+    receiver_user_id INT,
+    currency_id INT NOT NULL,
+    transaction_type_id INT NOT NULL,
+    FOREIGN KEY (sender_user_id) REFERENCES users(id),
+    FOREIGN KEY (receiver_user_id) REFERENCES users(id),
+    FOREIGN KEY (currency_id) REFERENCES currencies(id),
+    FOREIGN KEY (transaction_type_id) REFERENCES transaction_types(id)
+);
+
+-- INSERT DATA
+INSERT INTO
+  users (name, email, password, balance)
+VALUES
+  ('Felipe', 'felipe@example.com', '$2a$12$g.Bfz6u2Cg/KPPXP8ZFiLONclnUWmoEuq/ttNzFOBrsw3NdOEYctO', 100.00),
+  ('María', 'maria@example.com', '$2a$12$g.Bfz6u2Cg/KPPXP8ZFiLONclnUWmoEuq/ttNzFOBrsw3NdOEYctO', 150.00),
+  ('Pedro', 'pedro@example.com', '$2a$12$g.Bfz6u2Cg/KPPXP8ZFiLONclnUWmoEuq/ttNzFOBrsw3NdOEYctO', 200.00),
+  ('Ana', 'ana@example.com', '$2a$12$g.Bfz6u2Cg/KPPXP8ZFiLONclnUWmoEuq/ttNzFOBrsw3NdOEYctO', 50.00),
+  ('Elena', 'elena@example.com', '$2a$12$g.Bfz6u2Cg/KPPXP8ZFiLONclnUWmoEuq/ttNzFOBrsw3NdOEYctO', 75.00);
+
+INSERT INTO
+    roles (name)
+VALUES
+    ('Admin'), ('User');
+
+INSERT INTO
+    user_roles (user_id, role_id)
+VALUES
+    (1, 1), (2, 2), (3, 2), (4, 2), (5, 2);
+
+INSERT INTO
+  currencies (name, symbol)
+VALUES
+  ('Dólar estadounidense', 'US$'),
+  ('Euro', '€'),
+  ('Pesos Chilenos', '$');
+
+INSERT INTO
+    transaction_types (name)
+VALUES
+    ('Transferencia'), ('Depósito'), ('Retiro');
+
+INSERT INTO
+  transactions (sender_user_id, receiver_user_id, currency_id, transaction_type_id, total, transaction_date)
+VALUES
+  (1, 2, 1, 1, 50.00, '2024-03-17 08:30:00'),
+  (2, 3, 2, 1, 30.00, '2024-03-16 10:45:00'),
+  (3, 1, 3, 1, 20.00, '2024-03-15 14:20:00'),
+  (1, 3, 1, 1, 15.00, '2024-03-14 16:55:00'),
+  (2, 1, 2, 1, 25.00, '2024-03-13 09:10:00'),
+  (3, 2, 3, 1, 40.00, '2024-03-12 11:25:00'),
+  (4, 1, 1, 1, 10.00, '2024-03-11 13:30:00'),
+  (1, 4, 2, 1, 12.00, '2024-03-10 15:45:00'),
+  (4, 2, 3, 1, 18.00, '2024-03-09 17:00:00'),
+  (2, 4, 1, 1, 22.00, '2024-03-08 19:15:00');
+
+-- 1. Consulta para obtener el nombre de la moneda elegida por los usuarios en cada transacción:
+SELECT t.id AS transaction_id,
+       t.total,
+       t.transaction_date,
+       u_sender.name AS sender_name,
+       u_receiver.name AS receiver_name,
+       c.name AS currency_name,
+       tt.name AS type_transaction
+FROM transactions AS t
+JOIN users AS u_sender ON t.sender_user_id = u_sender.id
+JOIN users AS u_receiver ON t.receiver_user_id = u_receiver.id
+JOIN currencies c ON t.currency_id = c.id
+JOIN transaction_types tt ON t.transaction_type_id = tt.id
+ORDER BY transaction_id; -- Esta consulta muestra una tabla con las transacciones y monedas elegidas en cada una.
+
+-- 2. Consulta para obtener todas las transacciones registradas:
+SELECT *
+FROM transactions;
+
+-- 3. Consulta para obtener todas las transacciones realizadas por un usuario específico:
+SELECT t.id AS transaction_id,
+       sender.name AS sender_name,
+       receiver.name AS receiver_name,
+       t.total,
+       t.transaction_date,
+       c.name AS currency_name,
+       c.symbol AS currency_symbol,
+       tt.name AS type_transaction
+FROM transactions t
+JOIN users AS sender ON t.sender_user_id = sender.id
+JOIN users AS receiver ON t.receiver_user_id = receiver.id
+JOIN currencies c ON t.currency_id = c.id
+JOIN transaction_types tt ON t.transaction_type_id = tt.id
+WHERE t.sender_user_id = 1; -- Esta es la ID del usuario que hizo la transacción.
+
+-- 4. Sentencia para modificar el campo correo electrónico de un usuario específico:
+UPDATE users
+SET email = ''
+WHERE id = 1; -- Esta es la ID del usuario que se desea modificar su correo.
+
+-- 5. Sentencia para eliminar los datos de una transacción (eliminado de la fila completa):
+DELETE FROM transactions
+WHERE id = 1; -- Esta es la ID de la transacción que se desea borrar.
+
+-- 6. Consulta para ver los usuarios y sus roles
+SELECT u.name AS user_name, u.email AS user_email, r.name AS role FROM users AS u
+JOIN user_roles AS ur ON u.id = ur.user_id
+JOIN roles AS r ON ur.role_id = r.id;
